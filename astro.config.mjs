a02d17433d7c8aba74676d4dacae8ef79801a710
Arc655743 Orange <1343766898@qq.com>
2026-08-04 18:58:03 +0800

    fix: restore custom domain config. Custom domain required for base: /

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,8 +27,8 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://orangeartc0915.github.io/Hello-Mental-Omega-Launcher/",
-	base: "/Hello-Mental-Omega-Launcher/",
+	site: "https://mizuki.mysqil.com/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
